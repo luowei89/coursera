2dde1f390d7e4750b02dccddb22f8a97e0c93e59
Wei Luo <luowei@ccs.neu.edu>
2014-05-20 02:04:43 -0400

    ex1 submit

--- Machine Learning/mlclass-ex1-005/mlclass-ex1/gradientDescent.m
+++ Machine Learning/mlclass-ex1-005/mlclass-ex1/gradientDescent.m
@@ -16,12 +16,10 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
+    pd_0 = alpha * 1/m * sum(X*theta - y);
+    pd_1 = alpha * 1/m * sum((X*theta - y).*X(:,2));
+    pd_s = [pd_0 pd_1];
+    theta = theta - transpose(pd_s);
 
     % ============================================================
 
--- Machine Learning/mlclass-ex1-005/mlclass-ex1/gradientDescentMulti.m
+++ Machine Learning/mlclass-ex1-005/mlclass-ex1/gradientDescentMulti.m
@@ -16,16 +16,11 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
+    for i = 1:size(X,2)
+        ds(i) = alpha * 1/m * sum((X*theta - y).*X(:,i));
+    end
 
-
-
-
-
-
-
-
-
-
+    theta = theta-transpose(ds);
 
     % ============================================================
 
